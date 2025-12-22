66dd51a87966709a573eb99a323f136a87ec69a9
ismail710 <72998583+ismail710@users.noreply.github.com>
2025-12-22 16:09:09 +0100

    Refactor VideoRepository to use JpaRepository

--- mediaserver/src/main/java/org/example/mediaserver/dao/repositories/VideoRepository.java
+++ mediaserver/src/main/java/org/example/mediaserver/dao/repositories/VideoRepository.java
@@ -1,32 +1,13 @@
 package org.example.mediaserver.dao.repositories;
 
 import org.example.mediaserver.dao.entities.VideoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public class VideoRepository {
-    private final List<VideoEntity> videos = new ArrayList<>();
+public interface VideoRepository extends JpaRepository<VideoEntity,String> {
+    List<VideoEntity> findByCreatorId(String id);
 
-    public VideoEntity save(VideoEntity video) {
-        videos.add(video);
-        return video;
-    }
-
-    public Optional<VideoEntity> findById(String id) {
-        return videos.stream().filter(v -> v.getId().equals(id)).findFirst();
-    }
-
-    public List<VideoEntity> findByCreatorId(String creatorId) {
-        List<VideoEntity> result = new ArrayList<>();
-        for (VideoEntity video : videos) {
-            if (video.getCreator().getId().equals(creatorId)) {
-                result.add(video);
-            }
-        }
-        return result;
-    }
 }
