4dc2a72928f853811563f0f4d4d76f9c3554be69
ismail710 <72998583+ismail710@users.noreply.github.com>
2025-12-22 16:07:18 +0100

    Update UploadVideoDto.java

--- mediaclient/src/main/java/org/example/mediaclient/dto/UploadVideoDto.java
+++ mediaclient/src/main/java/org/example/mediaclient/dto/UploadVideoDto.java
@@ -1,12 +1,22 @@
 package org.example.mediaclient.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class UploadVideoDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String description;
+    @NotBlank
     private String url;
+    @Positive
     private int durationSeconds;
+    @Valid
+    @NotNull
     private CreatorDto creator;
 }
